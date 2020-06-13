a590d148fafedf3a2ecbc72284409c1d1788a65c
Dionathan Cordova <dionathan.cordova@vivitech.com.br>
2020-06-13 16:34:10 -0300

    celebrate

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { celebrate, Joi, Segments } from 'celebrate';
 import ForgotPasswordConstroller from '../controllers/ForgotPasswordController';
 import ResetPasswordConstroller from '../controllers/ResetPassowordController';
 
@@ -7,7 +7,27 @@ const passwordRouter = Router();
 const forgotPasswordConstroller = new ForgotPasswordConstroller();
 const resetPasswordConstroller = new ResetPasswordConstroller();
 
-passwordRouter.post('/forgot', forgotPasswordConstroller.create);
-passwordRouter.post('/reset', resetPasswordConstroller.create);
+passwordRouter.post(
+    '/forgot',
+    celebrate({
+        [Segments.BODY]: {
+            email: Joi.string().email().required(),
+        },
+    }),
+    forgotPasswordConstroller.create,
+);
+passwordRouter.post(
+    '/reset',
+    celebrate({
+        [Segments.BODY]: {
+            token: Joi.string().uuid().required(),
+            password: Joi.string().required(),
+            password_confirmation: Joi.string()
+                .required()
+                .valid(Joi.ref('password')),
+        },
+    }),
+    resetPasswordConstroller.create,
+);
 
 export default passwordRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 import UserRopository from '@modules/users/infra/typeorm/repositories/UsersRepository';
@@ -13,7 +14,17 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 const usersController = new UsersController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().required(),
+        },
+    }),
+    usersController.create,
+);
 
 usersRouter.patch(
     '/avatar',
